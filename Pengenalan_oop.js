1c782a02e26adcf34d335351943f0f76de4d1ec8
kadekframa <dframa19@gmail.com>
2021-12-03 03:42:40 +0800

    first commit JavaScript OOP

---
+++ Pengenalan_oop.js
@@ -0,0 +1,16 @@
+// Pengenalan Object Oriented Programming.
+// Object oriented programming adalah sudut pandang bahasa pemrograman yang berkonsep object.
+// Ada banyak sudut pandang bahasa pemrograman, namun OOP adalah yang sangat populer saat ini.
+// Ada beberapa istilah yang perlu dimengerti dalam OOP, yaitu: Object dan class.
+
+// Apa itu object ?
+// Object adalah data yang berisi field/ properties/ attributes dan method/function/behavior.
+
+//Apa itu class ?
+// Class adalah blueprint, prototype atau cetakan untuk membuat Object.
+// Class berisikan deklarasi semua properties dan functions yang dimiliki oleh Object.
+// Setiap Object selalu dibuat dari class.
+// Dan sebuah class bisa membuat object tanpa batas.
+
+// Javascript sendiri sebenarnya sejak awal dibuat sebagai bahasa prosedural, bukan bahasa pemrograman berorientasi objek.
+// Oleh karena, implementasi OOP di Javascript memang tidak sedetail bahasa pemrograman lain yang memang dari awal merupakan bahasa pemrograman OOP seperti Java atau C++.
