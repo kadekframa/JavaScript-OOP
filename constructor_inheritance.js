d923d5491f548bdd079eafecbcfc88a923e86f6c
kadekframa <dframa19@gmail.com>
2021-12-03 17:45:03 +0800

    JavaScript OOP learning by doing. Belajar cara penggunaan constructor inheritance pada JavaScript. Tetap Semangat Berlatih!

---
+++ constructor_inheritance.js
@@ -0,0 +1,28 @@
+
+// Cara penggunaan constructor inheritance pada JavaScript.
+// Dalam constructor kita biasanya membuat property baik itu berisi value ataupun function.
+// Di dalam constructor, kita bisa memanggil constructor lain, dengan begitu kita bisa mewarisi semua property yang dibuat di constructor lain tersebut.
+// Untuk memanggil constructor lain, kita bisa menggunakan NamaConstructor.call(this, parameter).
+
+// Contoh penggunaan.
+
+function Employee(firstName){
+    this.firstName = firstName;
+    this.sayello = function(name){
+        console.info(`Hi ${name}, my name is ${this.firstName}`);
+    }
+};
+
+
+function Manager(firstName, lastName){
+    Employee.call(this, firstName);         // contoh cara untuk memanggil constructor lain.
+    this.lastName = lastName;
+
+}
+
+const kadek = new Manager("Kadek", "Frama");
+console.info(kadek);
+
+
+
+// NB: Sederhananya kegunaan dari constructor inheritance ini yaitu adalah untuk mengcopy kode yang ada pada constructor lain ke constructor yang kita mau.
