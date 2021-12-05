2512a194d071ea0ed2c68713120d150682775358
kadekframa <dframa19@gmail.com>
2021-12-05 23:53:21 +0800

    JavaScript OOP learning by doing. Berlatih cara penggunaan static field pada JavaScript. Tetap Semangat Berlatih !!!, Pantang Menyerah!!!

---
+++ static_class_field.js
@@ -0,0 +1,11 @@
+
+// Static class field pada JavaScript.
+// Static adalah kata kunci yang bisa kita tambahkan sebelum field atau method, biasanya ketika kita membuat field atau method, maka secara otomatis field akan menjadi property di instace object, dan method akan menjadi function di prototype.
+// Jika kita tambahkan static, maka hal itu tidak terjadi.
+
+// Jika kita tambahkan static dalam class field, secara otomatis field tersebut bukan lagi milik instance object, melainkan milik class itu sendiri.
+// Biasanya static digunakan jika kita ingin membuat utility field atau function.
+// Cara mengakses static class field pun tidak lagi lewat object, melainkan lewat class nya.
+// Static class field bisa diartikan sifatnya global, tidak peduli diakses dimana atau siapa yang mengakses, hasilnya akan sama.
+
+// Contoh penggunaan static field.
