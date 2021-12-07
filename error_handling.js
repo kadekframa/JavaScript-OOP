cb78259e9b427f674472a4ce9eaea07ffe7179dc
kadekframa <dframa19@gmail.com>
2021-12-07 23:23:48 +0800

    JavaScript OOP learning by doing. Berlatih cara Error Handling pada JavaScript, yaitu dengan menggunakan try catch. Tetap Semangat Berlatih!!!

---
+++ error_handling.js
@@ -0,0 +1,10 @@
+
+// Error Handling pada JavaScript.
+// Saat terjadi eror di kode program JavaScript, kadang kita tidak ingin program kita berhenti.
+// Di JavaScript, kita bisa menagkap jika terjadi error.
+// Kita bisa menggunakan try catch statement untuk menangkap error.
+// Pada block try, kita akan mencoba mengakses kode program yang bisa menyebabkan error, dan jika terjadi error, block try akan berhenti dan otomatis masuk ke block catch.
+// Jika tidak terjadi error, block catch tidak akan dieksekusi.
+
+
+// Contoh penggunaan try cartch.
