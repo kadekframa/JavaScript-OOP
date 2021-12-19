16ae274bd29c357458f2bd0b81114c6089909468
kadekframa <dframa19@gmail.com>
2021-12-19 23:19:20 +0800

    JavaScript OOP learning by doing. Belajar cara penggunaan iterable dan iterator pada JavaScript. Tetap Semangat Berlatih!!!

--- iterable-iterator.js
+++ iterable-iterator.js
@@ -1,3 +1,7 @@
 
 // Iterable dan iterator pada JavaScript.
-// 
+// Salah satu fitur terbaru di ES6 adalah iterable.
+// Iterable adalah spesial object yang memiliki standarisasi.
+// Dengan mengikuti standarisasi Iterable, secara otomatis kita bisa melakukan iterasi terhadap data tersebut dengan menggunakan perulangan for..of.
+// Contoh yang sudah mengikuti standarisasi Iterable adalah String, Array, Object dan lain-lain.
+
